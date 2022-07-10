8532683b7dfa8ee947593df013cbe3b13e28ba7f
Emilio Reyes <soda480@gmail.com>
2022-07-10 14:19:11 -0700

    fix: set anybadge_use_shields default value
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- build.py
+++ build.py
@@ -19,7 +19,7 @@ name = 'pybuilder-anybadge'
 authors = [Author('Emilio Reyes', 'soda480@gmail.com')]
 summary = 'A pybuilder plugin that generates badges for your project'
 url = 'https://github.com/soda480/pybuilder-anybadge'
-version = '0.3.1'
+version = '0.3.2'
 default_task = [
     'clean',
     'analyze',
